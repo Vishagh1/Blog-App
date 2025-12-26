9fb9fc1c77101c552c4d144eec071adaee589007
Vishagh Sivankutty <vishaghsivankutty11@gmail.com>
2025-12-26 16:05:01 +0530

    Post Operations

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 # posts = [
 #     {
@@ -17,15 +19,57 @@ from .models import Post
 #     }
 # ]
 
-def home(request):
-    context = {
-        'posts':Post.objects.all()
-    }
-    return render(request,"blog/home.html",context)
+# def home(request):
+#     context = {
+#         'posts':Post.objects.all()
+#     }
+#     return render(request,"blog/home.html",context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+    success_url = reverse_lazy('blog-home')
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    success_url = reverse_lazy('blog-home')
+
+
 
 def about(request):
     context = {
         "title":"vishagh's blog"
     }
     return render(request, "blog/about.html",context)
-# Create your views here.
